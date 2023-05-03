5bea603f1856831df5805b2f7669f2d23c8a2c72
1amageek <nori@stamp.team>
2023-05-03 23:36:00 +0900

    Fix

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
             targets: ["Firestore"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.10.0"),
-        .package(url: "https://github.com/apple/swift-nio.git", from: "2.44.0"),
+        .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.17.0"),
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.51.1"),
         .package(url: "https://github.com/1amageek/FirebaseAPI.git", branch: "main"),
         .package(url: "https://github.com/vapor/jwt-kit.git", from: "4.0.0")
     ],
