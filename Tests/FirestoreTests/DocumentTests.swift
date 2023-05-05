cfd1bfbc2b14609fa1c6a2b6e2970ca6b1ce9a1b
1amageek <nori@stamp.team>
2023-05-05 17:15:06 +0900

    Fix

--- Tests/FirestoreTests/DocumentTests.swift
+++ Tests/FirestoreTests/DocumentTests.swift
@@ -9,32 +9,32 @@ final class DocumentTests: XCTestCase {
     }
 
     override func setUp() async throws {
-        let collection = Firestore.firestore().collection("test")
-
-        try await collection.document("doc")
-            .setData([
-                "number": 0,
-                "string": "string",
-                "bool": true,
-                "array": ["0", "1"],
-                "map": ["key": "value"],
-                "date": Date(timeIntervalSince1970: 0),
-                "timestamp": Timestamp(seconds: 0, nanos: 0),
-                "data": "data".data(using: .utf8)!,
-                "geoPoint": GeoPoint(latitude: 0, longitude: 0)
-            ])
-
-        try await collection.document("serverTimestamp")
-            .setData([
-                "serverTimestamp": FieldValue.serverTimestamp,
-            ])
+//        let collection = Firestore.firestore().collection("test")
+//
+//        try await collection.document("doc")
+//            .setData([
+//                "number": 0,
+//                "string": "string",
+//                "bool": true,
+//                "array": ["0", "1"],
+//                "map": ["key": "value"],
+//                "date": Date(timeIntervalSince1970: 0),
+//                "timestamp": Timestamp(seconds: 0, nanos: 0),
+//                "data": "data".data(using: .utf8)!,
+//                "geoPoint": GeoPoint(latitude: 0, longitude: 0)
+//            ])
+//
+//        try await collection.document("serverTimestamp")
+//            .setData([
+//                "serverTimestamp": FieldValue.serverTimestamp,
+//            ])
     }
 
     override func tearDown() async throws {
-        let firestore = Firestore.firestore()
-        let collection = firestore.collection("test")
-        try await collection.document("doc").delete()
-        try await collection.document("serverTimestamp").delete()
+//        let firestore = Firestore.firestore()
+//        let collection = firestore.collection("test")
+//        try await collection.document("doc").delete()
+//        try await collection.document("serverTimestamp").delete()
     }
 
     class func loadServiceAccount(from jsonFile: String) throws -> ServiceAccount {
@@ -51,40 +51,41 @@ final class DocumentTests: XCTestCase {
         }
     }
 
-    func testEmptyDocument() async throws {
-        let snapshot = try await Firestore
-            .firestore()
-            .collection("test")
-            .document("empty")
-            .getDocument()
-        XCTAssertEqual(snapshot.isEmpty, true)
-    }
-
-    func testDocumentData() async throws {
-        let snapshot = try await Firestore
-            .firestore()
-            .collection("test")
-            .document("doc")
-            .getDocument()
-        let data = snapshot.data()!
-        XCTAssertEqual(data["number"] as! Int, 0)
-        XCTAssertEqual(data["string"] as! String, "string")
-        XCTAssertEqual(data["bool"] as! Bool, true)
-        XCTAssertEqual(data["array"] as! [String], ["0", "1"])
-        XCTAssertEqual(data["map"] as! [String: String], ["key": "value"])
-        XCTAssertEqual(data["date"] as! Timestamp, Timestamp(seconds: 0, nanos: 0))
-        XCTAssertEqual(data["timestamp"] as! Timestamp, Timestamp(seconds: 0, nanos: 0))
-        XCTAssertEqual(data["geoPoint"] as! GeoPoint, GeoPoint(latitude: 0, longitude: 0))
-    }
-
-    func testSeriverTimestamp() async throws {
-        let snapshot = try await Firestore
+    func testServerTimestamp() async throws {
+        let ref = Firestore
             .firestore()
             .collection("test")
             .document("serverTimestamp")
-            .getDocument()
+
+        try await ref.setData([
+            "serverTimestamp": FieldValue.serverTimestamp
+        ])
+        let snapshot = try await ref.getDocument()
         let data = snapshot.data()!
         XCTAssertTrue(data["serverTimestamp"] is Timestamp)
+    }
+
+
+    func testConvertTimestampToDate() async throws {
+
+        struct TimestampObject: Codable {
+            var value: Timestamp = Timestamp(seconds: 1000, nanos: 0)
+        }
+
+        struct DateObject: Codable {
+            var value: Date
+        }
+
+        let ref = Firestore
+            .firestore()
+            .collection("test")
+            .document("testConvertTimestampToDate")
+        let writeData = TimestampObject()
+        try await ref.setData(writeData)
+        let readData = try await ref.getDocument(type: DateObject.self)
+
+        XCTAssertEqual(writeData.value.seconds, Int64(readData!.value.timeIntervalSince1970))
+
     }
 
     func testRoundtrip() async throws {
@@ -164,7 +165,7 @@ final class DocumentTests: XCTestCase {
             .firestore()
             .collection("test")
             .document("roundtrip")
-                try await ref.setData(writeData)
+        try await ref.setData(writeData)
         let readData = try await ref.getDocument(type: Object.self)
 
         XCTAssertEqual(writeData.number, readData!.number)
